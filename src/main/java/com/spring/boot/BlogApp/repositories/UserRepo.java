b6b1074cef73253056e516bec2d20f9d32887e64
mrithunjay98 <119282148+mrithunjay98@users.noreply.github.com>
2024-12-24 10:23:07 +0530

    Delete src directory

--- src/main/java/com/spring/boot/BlogApp/repositories/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.spring.boot.BlogApp.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.spring.boot.BlogApp.entities.User;
-
-public interface UserRepo extends JpaRepository<User,Integer>{
-
-}
--- src/main/java/com/spring/boot/BlogApp/test/GitCloneChanges.java
+++
@@ -1,9 +0,0 @@
-package com.spring.boot.BlogApp.test;
-
-public class GitCloneChanges {
-	
-	public void m1() {
-		System.out.println("Happy New Year");
-	}
-}
-
